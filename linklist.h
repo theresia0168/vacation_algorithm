800e62ed7f69d88ad0a5e9655480241d786fa803
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2022-08-19 16:34:32 +0900

    Create linklist.h

---
+++ linklist.h
@@ -0,0 +1,53 @@
+#ifndef __LINKLIST_H__
+#define __LINKLIST_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+        char value;
+        struct node *next;
+}NODE;
+
+NODE *node_new(char p){
+        NODE *buf = NULL;
+        buf = (NODE*)malloc(sizeof(NODE)*1);
+        if(buf==NULL)
+                return NULL;
+        buf->value = p;
+        buf->next = NULL;
+        return buf;
+}
+
+NODE *node_append(NODE *n, char p){
+        NODE *buf = NULL;
+        buf = (NODE*)malloc(sizeof(NODE)*1);
+        buf->value = p;
+        while(n->next!=NULL){
+                n = n->next;
+        }
+        n->next = buf;
+        return buf;
+}
+
+void node_print(NODE *n){
+        printf("Node value is \'%c\'\n", n->value);
+}
+
+void node_print_link(NODE *n){
+        NODE *next1 = n;
+        while(1){
+                printf("%c",next1->value);
+                if(next1->next!=NULL)
+                        printf(" - ");
+                else
+                        break;
+                next1 = next1->next;
+        }
+        printf("\n");
+}
+
+void node_free(NODE *n){
+        free(n);
+}
+
+#endif
