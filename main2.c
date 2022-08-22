c8b02edb2c728864150dc2dd4f6ea844afe6d23e
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2022-08-22 16:58:29 +0900

    Update main2.c

--- main2.c
+++ main2.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char *argv[]){
 
-        LINKLIST mylist = NULL;
+        LINKLIST *mylist;
 
         mylist = linklist_new();
-        linklist_append(&mylist, 'a');
-        linklist_prepend(&mylist, 'b');
+        linklist_append(mylist, 'a');
+        linklist_prepend(mylist, 'b');
+        linklist_append(mylist, 'c');
+        linklist_append(mylist, 'd');
+        linklist_prepend(mylist, 's');
 
-        linklist_print(&mylist);        // b - a
+        linklist_print(mylist); // b - a
 
         linklist_delete(mylist);
 
